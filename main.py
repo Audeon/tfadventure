e9d0bd1d58a7487a5368d73e0773616a6d98a46a
Audeon <whitford.michael@gmail.com>
2022-01-25 11:05:06 -0600

    Continued working through classification problem on TF site. Added plots for the predictions.

--- main.py
+++ main.py
@@ -16,7 +16,6 @@ print(f"len(train_labels): {len(train_labels)}")
 
 print(f"Each train label is between 0 - 9: {train_labels}")
 
-
 # Lets preproccess the data
 
 # Graph time!
@@ -32,9 +31,9 @@ test_images = test_images / 255.0
 
 # Now verify the data is correct and that your ready to build a NN - Display the first 25 images.
 
-plt.figure(figsize=(10,10))
+plt.figure(figsize=(10, 10))
 for i in range(25):
-    plt.subplot(5,5, i+1)
+    plt.subplot(5, 5, i + 1)
     plt.xticks([])
     plt.yticks()
     plt.grid(False)
@@ -46,14 +45,99 @@ plt.show()
 # Lets build the layers of the Nerual network
 
 model = tf.keras.Sequential([
-    tf.keras.layers.Flatten(input_shape=(28,28)),
+    tf.keras.layers.Flatten(input_shape=(28, 28)),
     tf.keras.layers.Dense(128, activation='relu'),
+    tf.keras.layers.Dense(64, activation='relu'),
     tf.keras.layers.Dense(10)
 ])
 
 # Compiling the model and adding a loss function optimizer and metrics
 model.compile(optimizer='adam',
-             loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
-             metrics=['accuracy'])
+              loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
+              metrics=['accuracy'])
 
 model.fit(train_images, train_labels, epochs=10)
+
+# Making predictions - Logits are the vector of raw (non-normalized) predictions that a classifiction model generates.
+
+probability_model = tf.keras.Sequential([
+    model, tf.keras.layers.Softmax()])
+
+predictions = probability_model.predict(test_images)
+
+print(f"The prediction[0] is expected to be: {clas_names[int(np.argmax(predictions[0]))]}")
+
+
+# Lets build some functions to graph the 10 class predictions.
+def plot_image(i, predictions_array, true_label, img):
+    true_label, img = true_label[i], img[i]
+    plt.grid(False)
+    plt.xticks([])
+    plt.yticks([])
+
+    plt.imshow(img, cmap=plt.cm.binary)
+
+    predicted_label = np.argmax(predictions_array)
+
+    if predicted_label == true_label:
+        color = 'blue'
+    else:
+        color = 'red'
+
+    plt.xlabel("{} {:2.0f}% ({})".format(clas_names[predicted_label],
+                                         100*np.max(predictions_array),
+                                         clas_names[true_label]), color=color)
+
+def plot_value_array(i, predictions_array, true_label):
+    true_label = true_label[i]
+    plt.grid(False)
+    plt.xticks(range(10))
+    plt.yticks([])
+    thisplot = plt.bar(range(10), predictions_array, color="#777777")
+    plt.ylim([0, 1])
+    predicted_label = np.argmax(predictions_array)
+
+    thisplot[predicted_label].set_color('red')
+    thisplot[true_label].set_color('blue')
+
+# Verify and plot the predictions using the above functions.
+i = 0
+plt.figure(figsize=(6,3))
+plt.subplot(1,2,1)
+plot_image(i, predictions[i], test_labels, test_images)
+plt.subplot(1,2,2)
+plot_value_array(i, predictions[i], test_labels)
+plt.show()
+
+i = 12
+plt.figure(figsize=(6,3))
+plt.subplot(1,2,1)
+plot_image(i, predictions[i], test_labels, test_images)
+plt.subplot(1,2,2)
+plot_value_array(i, predictions[i], test_labels)
+plt.show()
+
+i = 3
+plt.figure(figsize=(6,3))
+plt.subplot(1,2,1)
+plot_image(i, predictions[i], test_labels, test_images)
+plt.subplot(1,2,2)
+plot_value_array(i, predictions[i], test_labels)
+plt.show()
+
+# Plot the first x test images their predicted labels and their true labels.
+# Also color the correct prdictions in blue, incorrect in red
+
+num_rows = 5
+num_cols = 3
+num_imgs = num_rows*num_cols
+
+plt.figure(figsize=(2*2*num_cols, 2*num_rows))
+for i in range(num_imgs):
+    plt.subplot(num_rows, 2*num_cols, 2*i+1)
+    plot_image(i, predictions[i], test_labels, test_images)
+    plt.subplot(num_rows, 2*num_cols, 2*i+2)
+    plot_value_array(i, predictions[i], test_labels)
+
+plt.tight_layout()
+plt.show()
