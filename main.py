6cce6631558004580f0cdd25d12db64f6041acbc
Audeon <whitford.michael@gmail.com>
2022-01-26 21:59:46 -0600

    Started work on text classification. Moved the clothing classification to its own file, for tinkering with later. Moved the flower and linearregression data to the ./data directory. I also added the ./data directory to .gitignore so we dont have a million data files going to the repo.

--- main.py
+++ main.py
@@ -1,143 +1,45 @@
-import tensorflow as tf
-
+import os
+import re
+import shutil
 import numpy as np
 import matplotlib.pyplot as plt
 
+import tensorflow as tf
+from tensorflow.keras import layers, losses
+
 print(tf.__version__)
 
-fashion_mnist = tf.keras.datasets.fashion_mnist
-(train_images, train_labels), (test_images, test_labels) = fashion_mnist.load_data()
 
-clas_names = ['T-shirt/top', 'Trouser', 'Pullover', 'Dress', 'Coat', 'Sandal', 'Shirt', 'Sneaker', 'Bag', 'Ankle boot']
+url = "https://ai.stanford.edu/~amaas/data/sentiment/aclImdb_v1.tar.gz"
 
-# Exploring the format of the data
-print(f"train_images.shape: {train_images.shape}")
-print(f"len(train_labels): {len(train_labels)}")
+cache_dir = "./data"
 
-print(f"Each train label is between 0 - 9: {train_labels}")
+if not os.listdir(cache_dir):
+    dataset = tf.keras.utils.get_file("aclImdb_v1", url, untar=True, cache_dir=cache_dir, cache_subdir='')
 
-# Lets preproccess the data
+dataset_dir = os.path.join(cache_dir, 'aclImdb')
+train_dir = os.path.join(dataset_dir, "train")
 
-# Graph time!
-plt.figure()
-plt.imshow(train_images[0])
-plt.colorbar()
-plt.grid(False)
-plt.show()
+# Lets read one for fun
+sample_file = os.path.join(train_dir, 'pos/11_9.txt')
+with open(sample_file) as f:
+    print(f.read())
 
-# We are scaling the values of these images to a range of 0 to 1.
-train_images = train_images / 255.0
-test_images = test_images / 255.0
+# Now we need to remove the data directories we are not interested in, in this case the unsup reviews.
+remove_dir = os.path.join(train_dir, 'unsup')
+if os.path.lexists(remove_dir):
+    shutil.rmtree(remove_dir)
 
-# Now verify the data is correct and that your ready to build a NN - Display the first 25 images.
+# Next we are going to split up the training dataset in order to have a training set and a validation set as we
+# arleady have atest set from this data
 
-plt.figure(figsize=(10, 10))
-for i in range(25):
-    plt.subplot(5, 5, i + 1)
-    plt.xticks([])
-    plt.yticks()
-    plt.grid(False)
-    plt.imshow(train_images[i], cmap=plt.cm.binary)
-    plt.xlabel(clas_names[train_labels[i]])
+batch_size = 32
+seed = 42
 
-plt.show()
-
-# Lets build the layers of the Nerual network
-
-model = tf.keras.Sequential([
-    tf.keras.layers.Flatten(input_shape=(28, 28)),
-    tf.keras.layers.Dense(128, activation='relu'),
-    tf.keras.layers.Dense(64, activation='relu'),
-    tf.keras.layers.Dense(10)
-])
-
-# Compiling the model and adding a loss function optimizer and metrics
-model.compile(optimizer='adam',
-              loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
-              metrics=['accuracy'])
-
-model.fit(train_images, train_labels, epochs=10)
-
-# Making predictions - Logits are the vector of raw (non-normalized) predictions that a classifiction model generates.
-
-probability_model = tf.keras.Sequential([
-    model, tf.keras.layers.Softmax()])
-
-predictions = probability_model.predict(test_images)
-
-print(f"The prediction[0] is expected to be: {clas_names[int(np.argmax(predictions[0]))]}")
-
-
-# Lets build some functions to graph the 10 class predictions.
-def plot_image(i, predictions_array, true_label, img):
-    true_label, img = true_label[i], img[i]
-    plt.grid(False)
-    plt.xticks([])
-    plt.yticks([])
-
-    plt.imshow(img, cmap=plt.cm.binary)
-
-    predicted_label = np.argmax(predictions_array)
-
-    if predicted_label == true_label:
-        color = 'blue'
-    else:
-        color = 'red'
-
-    plt.xlabel("{} {:2.0f}% ({})".format(clas_names[predicted_label],
-                                         100*np.max(predictions_array),
-                                         clas_names[true_label]), color=color)
-
-def plot_value_array(i, predictions_array, true_label):
-    true_label = true_label[i]
-    plt.grid(False)
-    plt.xticks(range(10))
-    plt.yticks([])
-    thisplot = plt.bar(range(10), predictions_array, color="#777777")
-    plt.ylim([0, 1])
-    predicted_label = np.argmax(predictions_array)
-
-    thisplot[predicted_label].set_color('red')
-    thisplot[true_label].set_color('blue')
-
-# Verify and plot the predictions using the above functions.
-i = 0
-plt.figure(figsize=(6,3))
-plt.subplot(1,2,1)
-plot_image(i, predictions[i], test_labels, test_images)
-plt.subplot(1,2,2)
-plot_value_array(i, predictions[i], test_labels)
-plt.show()
-
-i = 12
-plt.figure(figsize=(6,3))
-plt.subplot(1,2,1)
-plot_image(i, predictions[i], test_labels, test_images)
-plt.subplot(1,2,2)
-plot_value_array(i, predictions[i], test_labels)
-plt.show()
-
-i = 3
-plt.figure(figsize=(6,3))
-plt.subplot(1,2,1)
-plot_image(i, predictions[i], test_labels, test_images)
-plt.subplot(1,2,2)
-plot_value_array(i, predictions[i], test_labels)
-plt.show()
-
-# Plot the first x test images their predicted labels and their true labels.
-# Also color the correct prdictions in blue, incorrect in red
-
-num_rows = 5
-num_cols = 3
-num_imgs = num_rows*num_cols
-
-plt.figure(figsize=(2*2*num_cols, 2*num_rows))
-for i in range(num_imgs):
-    plt.subplot(num_rows, 2*num_cols, 2*i+1)
-    plot_image(i, predictions[i], test_labels, test_images)
-    plt.subplot(num_rows, 2*num_cols, 2*i+2)
-    plot_value_array(i, predictions[i], test_labels)
-
-plt.tight_layout()
-plt.show()
+raw_train_ds = tf.keras.utils.text_dataset_from_directory(
+    train_dir,
+    batch_size=batch_size,
+    validation_split=0.2,
+    subset='training',
+    seed=seed
+)
