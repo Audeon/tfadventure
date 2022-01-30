57462d1b9bbfed6d79a55f637111d6b6c38eba06
Audeon <whitford.michael@gmail.com>
2022-01-29 18:53:58 -0600

    Continued work on text classification and keras.

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import os
 import re
 import shutil
+import string
+
 import numpy as np
 import matplotlib.pyplot as plt
 
@@ -19,7 +21,7 @@ if not os.listdir(cache_dir):
 
 dataset_dir = os.path.join(cache_dir, 'aclImdb')
 train_dir = os.path.join(dataset_dir, "train")
-
+test_dir = os.path.join(dataset_dir, 'test')
 # Lets read one for fun
 sample_file = os.path.join(train_dir, 'pos/11_9.txt')
 with open(sample_file) as f:
@@ -43,3 +45,105 @@ raw_train_ds = tf.keras.utils.text_dataset_from_directory(
     subset='training',
     seed=seed
 )
+
+# Now that we have it stored in tf.data we can iterate through it.
+for text_batch, label_batch in raw_train_ds.take(1):
+    for i in range(3):
+        print("Review: ", text_batch.numpy()[i])
+        print("Label: ", label_batch.numpy()[i])
+
+print("Label 0 corresponds to", raw_train_ds.class_names[0])
+print("Label 1 corresponds to", raw_train_ds.class_names[1])
+
+# Now we are going to take the remaining documents and create the validation dataset. And the test dataset.
+raw_val_ds = tf.keras.utils.text_dataset_from_directory(
+    train_dir,
+    batch_size=batch_size,
+    validation_split=0.2,
+    subset='validation',
+    seed=seed )
+
+raw_test_ds = tf.keras.utils.text_dataset_from_directory(
+    test_dir,
+    batch_size=batch_size )
+
+# Now we build a function to strip the HTML and punctuation from the input data.
+def custom_standardization(input_data):
+    lowercase = tf.strings.lower(input_data)
+    stripped_html = tf.strings.regex_replace(lowercase, '<br />', ' ')
+    return tf.strings.regex_replace(stripped_html, '[%s]' % re.escape(string.punctuation), '')
+
+# Now we are goign to create the TextVectorization layer which will standardize, tokenize, and vectorize the data for the NN.
+max_features = 10000
+sequence_lenght = 250
+
+vectorize_layer = layers.TextVectorization(
+    standardize=custom_standardization,
+    max_tokens=max_features,
+    output_mode='int',
+    output_sequence_length=sequence_lenght )
+
+# Make a text-only dataset (without labels), then call adapt.
+train_text = raw_train_ds.map(lambda x, y: x)
+vectorize_layer.adapt(train_text)
+
+# Now lets visualize some data.
+def vectorize_text(text, label):
+    text = tf.expand_dims(text, -1)
+    return vectorize_layer(text), label
+
+# Grab a batch of 32 reviews and labels from the dataset
+view_tb, view_lb = next(iter(raw_train_ds))
+first_review, first_label = view_tb[0], view_lb[0]
+print("Review: ", first_review)
+print("Label", raw_train_ds.class_names[first_label])
+print("Vectorized review", vectorize_text(first_review, first_label))
+
+# We can look at what the vocabularay is for each of the above vectorized ints.
+print("85 ---> ",vectorize_layer.get_vocabulary()[85])
+print("17 ---> ",vectorize_layer.get_vocabulary()[17])
+print("260 ---> ",vectorize_layer.get_vocabulary()[260])
+print("2 ---> ",vectorize_layer.get_vocabulary()[2])
+print('Vocabulary size: {}'.format(len(vectorize_layer.get_vocabulary())))
+
+# Now lets apply the same textvectorization layer we created
+train_ds = raw_train_ds.map(vectorize_text)
+val_ds = raw_val_ds.map(vectorize_text)
+test_ds = raw_test_ds.map(vectorize_text)
+
+# Now configure the datasets for performance. We can cache them in memory to ensure loading the dataset is not the
+# bottle neck of training it. We will use prefetch to overlap data processing and model execution while training.
+AUTOTUNE = tf.data.AUTOTUNE
+train_ds = train_ds.cache().prefetch(buffer_size=AUTOTUNE)
+val_ds = test_ds.cache().prefetch(buffer_size=AUTOTUNE)
+test_ds = test_ds.cache().prefetch(buffer_size=AUTOTUNE)
+
+# Now to create the model for the Neural Network
+embedding_dim = 16
+model = tf.keras.Sequential([
+    layers.Embedding(max_features + 1, embedding_dim),
+    layers.Dropout(0.2),
+    layers.GlobalAveragePooling1D(),
+    layers.Dropout(0.2),
+    layers.Dense(1)])
+
+model.summary()
+
+# Loss function and optimization - This is a binary classification problem so we will need a loss function and optimizer.
+model.compile(loss=losses.BinaryCrossentropy(from_logits=True),
+              optimizer='adam',
+              metrics=tf.metrics.BinaryAccuracy(threshold=0.0))
+
+# Now we train the model
+epochs = 10
+history = model.fit(
+    train_ds,
+    validation_data=val_ds,
+    epochs=epochs
+)
+
+# Eval the model
+
+loss, accuracy = model.evaluate(test_ds)
+print("Loss: ", loss)
+print("Accuracy: ", accuracy)
